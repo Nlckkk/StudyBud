c312680b2f609f4999c8ea36c04fe58afca4eef1
[Nlckkk] <[nalejandro4848@gmail.com]>
2025-02-16 21:04:50 -0500

    Static Files

--- base/views.py
+++ base/views.py
@@ -84,8 +84,6 @@ def home(request):
     room_count = rooms.count()
     # room_messages = Message.objects.all()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
-    context = {'rooms': rooms, 'topics': topics, 
-               'room_count': room_count, 'room_messages': room_messages}
     context = {'rooms': rooms, 'topics': topics,'room_count': room_count, 
                'room_messages': room_messages}
     return render(request, 'base/home.html', context)
@@ -123,17 +121,28 @@ def userProfile(request, pk):
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     topics = Topic.objects.all() 
+
     context = {'user': user, 'rooms': rooms,'room_messages': room_messages, 
                'topics': topics}
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics':topics}
     return render(request, 'base/profile.html', context)
+
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
+            
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
 
