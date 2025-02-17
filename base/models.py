51b7b5d5e49bd0ae115fbb8d1c80b9a7a4d5b6e9
[Nlckkk] <[nalejandro4848@gmail.com]>
2025-02-16 20:22:15 -0500

    Flash Messages

--- base/models.py
+++ base/models.py
@@ -29,5 +29,9 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.body[0:50]
+
+class Meta:
+    ordering = ['-updated','-created']
+
+def __str__(self):
+    return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages 
 from .models import Room
+from django.db.models import Q
+from django.contrib.auth.models import User
+from .forms import RoomForm
 
 # Create your views here.
 
